d03875809908c374f7591b088e4f5556ee384d18
CoooperEisman <cooper@eisman.me>
2021-05-12 13:18:02 -0600

    Get, put and Contains complete

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,9 +1,88 @@
 public class BinarySearchTree<Key extends Comparable<Key>, Value> {
 
-    public Node<Key, Value> root;
+    private Node<Key, Value> root;
+    private int length;
 
     public BinarySearchTree() {
-
+        root = new Node<Key, Value>(null,null);
+        length = 0;
     }
+
+    //Test if the Tree is Empty
+    public boolean isEmpty() {
+        return length <= 0;
+    }
+
+    //Returns True if the Key is contained in the Array
+    public boolean contains(Key key) {
+        return (get(key) != null);
+    }
+
+    //Returns Length
+    public int getLength() {
+        return length;
+    }
+
+    //Wrapper for Recursive Put
+    public void put(Key key, Value value) {
+        if(isEmpty()) {
+            root = new Node<Key, Value>(key, value);
+            length++;
+        } else {
+            if(put(root,key,value)) {
+                length++;
+            }
+        }
+    }
+
+    //Puts a value into an Array. If it is a new value, return true. If Key exists, return false;
+    private boolean put(Node node,Key key, Value value) {
+        if(node.getKey().compareTo(key) > 0) {
+            if(node.hasRight()) {
+                return put(node.getRight(),key,value);
+            } else {
+                node.setRight(new Node(key,value));
+                return true;
+            }
+        } else if(node.getKey().compareTo(key) < 0) {
+            if(node.hasLeft()) {
+                return put(node.getLeft(),key,value);
+            } else {
+                node.setLeft(new Node(key,value));
+                return true;
+            }
+        } else {
+            node.setValue(value);
+            return false;
+        }
+    }
+
+    //Wrapper for Recursive get
+    public Value get(Key key) {
+        if (isEmpty()) {
+            return null;
+        }
+        return get(root, key);
+    }
+
+    //Recursive Get. Returns null without value
+    private Value get(Node<Key,Value> node,Key key) {
+        if(node.getKey().compareTo(key) > 0) {
+            if(node.hasRight()) {
+                return get(node.getRight(),key);
+            } else {
+                return null;
+            }
+        } else if(node.getKey().compareTo(key) < 0) {
+            if(node.hasLeft()) {
+                return get(node.getLeft(),key);
+            } else {
+                return null;
+            }
+        } else {
+            return node.getValue();
+        }
+    }
+
 
 }
--- src/main.java
+++ src/main.java
@@ -1,11 +1,20 @@
+import com.sun.jdi.Value;
+
+import java.security.Key;
+
 public class main {
     public static void main(String[] args) {
         BinarySearchTree<String, String> b = new BinarySearchTree<String, String>();
 
-        System.out.println("Size: " + b.size() + " IsEmpty: " + b.isEmpty());
-        b.put("Hello","World");
-        System.out.println("Size: " + b.size() + " IsEmpty: " + b.isEmpty());
-        b.put("Mario","Cart");
-        System.out.println("Size: " + b.size() + " IsEmpty: " + b.isEmpty());
+        System.out.println("IsEmpty: " + b.isEmpty() + " Length: " + b.getLength());
+        b.put("Hello", "World");
+        System.out.println("IsEmpty: " + b.isEmpty() + " Length: " + b.getLength());
+        b.put("Hello", "World");
+        System.out.println("IsEmpty: " + b.isEmpty() + " Length: " + b.getLength());
+        b.put("Cooper", "Eisman");
+        System.out.println("IsEmpty: " + b.isEmpty() + " Length: " + b.getLength());
+        System.out.println(b.get("Cooper"));
+        System.out.println(b.contains("Coop"));
+
     }
 }
